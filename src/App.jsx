4555111432cf4258feaad2e2f8eba26a7252f348
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-14 18:42:45 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header'; 
 function App() {
   return (
     <>
       <Header />
       <h1>React portfolio</h1>
+      <Footer/>
     </>
   );
 }
