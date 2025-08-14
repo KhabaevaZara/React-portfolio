c4263d055982b0c811ffde84902d435f59ddecd9
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-14 19:10:51 +0300

    1

--- src/components/HeaderMe/HeaderMe.jsx
+++ src/components/HeaderMe/HeaderMe.jsx
@@ -2,18 +2,18 @@ import './HeaderMe.css'
 
 const Header = () => {
   return (
-    <header class="header">
-    <div class="header__wrapper">
-        <h6 class="header__title">
+    <header className="header">
+    <div className="header__wrapper">
+        <h6 className="header__title">
             <strong data-i18n="header_title">Hi, I'm Zara</strong>
             <span data-i18n="header_subtitle">Frontend Developer & UI Designer</span>
         </h6>
-        <div class="header__text">
+        <div className="header__text">
             <p data-i18n="header_description">I create beautiful, responsive websites</p>
         </div>
-        <div class="header__actions">
-            <a href="#projects" class="btn" data-i18n="btn_projects">View Projects</a>
-            <a href="#contact" class="btn" data-i18n="btn_contact">Contact Me</a>
+        <div className="header__actions">
+            <a href="#projects" className="btn" data-i18n="btn_projects">View Projects</a>
+            <a href="#contact" className="btn" data-i18n="btn_contact">Contact Me</a>
         </div>
     </div>
    
