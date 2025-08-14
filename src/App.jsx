b0d5fe9a89d83fbfb5fd2ca55184cf69a186e62a
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-14 18:49:03 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header'; 
+import About from './components/About/About';
 function App() {
   return (
     <>
       <Header />
       <h1>React portfolio</h1>
+      <About/>
       <Footer/>
     </>
   );
---
+++ src/components/About/About.jsx
@@ -0,0 +1,43 @@
+import './About.css';
+
+const Home = () => {
+    return (
+      <section className="section about" id="about">
+          <div className="container">
+              <h2 className="title-1" data-i18n="about_title">About Me</h2>
+              <div className="about__content">
+                  <div className="about__image animate">
+                      <img src="" alt="Zara - Frontend Developer" />
+                  </div>
+                  <div className="about__info">
+                      <h2 data-i18n="about_heading">Frontend Developer with 5+ Years of Experience</h2>
+                      
+                      <div className="about__skills">
+                          <div className="skill-bar">
+                              <div className="skill-info">
+                                  <span data-i18n="skill_html">HTML & CSS</span>
+                                  <span>90%</span>
+                              </div>
+                              <div className="skill-progress">
+                                  <span style={{ width: '95%' }}></span>
+                              </div>
+                          </div>
+                          
+                          <div className="skill-bar">
+                              <div className="skill-info">
+                                  <span data-i18n="skill_js">JavaScript</span>
+                                  <span>80%</span>
+                              </div>
+                              <div className="skill-progress">
+                                  <span style={{ width: '90%' }}></span>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+    )
+  }
+  
+  export default Home
