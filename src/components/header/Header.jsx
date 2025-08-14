682e903eabf03a86a42f4a623073edad5ff76f8c
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-14 16:32:33 +0300

    1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,52 +1,46 @@
-
 import './Header.css';
-
+import { FaCode, FaBars, FaGlobe } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <nav class="nav" id="navbar">
-    <div class="container">
-        <div class="nav-row">
-            <a href="#" class="logo">
-                <i class="fas fa-code"></i>
-                <strong data-i18n="logo">Portfolio</strong>
-                
-            </a>
-            <div class="contener">
-            <ul class="nav-list" id="navMenu">
-                <li class="nav-list__item">
-                    <a href="#" class="nav-list__link " data-i18n="nav_home">Home</a>
-                </li>    
-                <li class="nav-list__item">
-                    <a href="#about" class="nav-list__link" data-i18n="nav_about">About</a>
-                </li> 
-                <li class="nav-list__item">
-                    <a href="#projects" class="nav-list__link" data-i18n="nav_projects">Projects</a>
-                </li> 
-                <li class="nav-list__item">
-                    <a href="#contact" class="nav-list__link" data-i18n="nav_contact">Contact</a>
-                </li> 
-  
+    <nav className="nav" id="navbar">
+      <div className="container">
+        <div className="nav-row">
+          <a href="#" className="logo">
+            <FaCode className="fas fa-code" />
+            <strong data-i18n="logo">Portfolio</strong>
+          </a>
+          <div className="contener">
+            <ul className="nav-list" id="navMenu">
+              <li className="nav-list__item">
+                <a href="#" className="nav-list__link" data-i18n="nav_home">Home</a>
+              </li>    
+              <li className="nav-list__item">
+                <a href="#about" className="nav-list__link" data-i18n="nav_about">About</a>
+              </li> 
+              <li className="nav-list__item">
+                <a href="#projects" className="nav-list__link" data-i18n="nav_projects">Projects</a>
+              </li> 
+              <li className="nav-list__item">
+                <a href="#contact" className="nav-list__link" data-i18n="nav_contact">Contact</a>
+              </li> 
             </ul>  
             
-            <button class="mobile-menu-btn" id="mobileMenuBtn">
-                <i class="fas fa-bars"></i>
+            <button className="mobile-menu-btn" id="mobileMenuBtn">
+              <FaBars className="fas fa-bars" />
             </button>
 
-            <div class="lang-switcher">
-                <button id="languageSwitcher" class="lang-btn">
-                    <i class="fas fa-globe"></i>
-                    <span id="langText">EN</span>
-                </button>
+            <div className="lang-switcher">
+              <button id="languageSwitcher" className="lang-btn">
+                <FaGlobe className="fas fa-globe" />
+                <span id="langText">EN</span>
+              </button>
             </div>
-
-            </div>
-
-
+          </div>
         </div>
-    </div>
-</nav>
-  )
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
