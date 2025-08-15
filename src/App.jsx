2210da78a02d9b56d101f5dd9643ee9d2772852e
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-15 14:31:30 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header'; 
-import About from './components/About/About';
-import HeaderMe from './components/HeaderMe/HeaderMe';
+import About from './components/about/About';
+import HeaderMe from './components/headerMe/HeaderMe';
 import Services from './components/service/Services';
 function App() {
   return (
