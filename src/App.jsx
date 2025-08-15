fa4c4d9a1b58def81984dcebedf6e6d2cabc4744
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-15 14:25:52 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Header from './components/header/Header';
 import About from './components/About/About';
 import HeaderMe from './components/HeaderMe/HeaderMe';
 import Services from './components/service/Services';
-
+import JavaScript from './components/JavaScript/translations'; 
 function App() {
   return (
     <>
+    <JavaScript/>
       <Header />
       <HeaderMe/>
       <About/>
