d16008728413f5b332cdba3fa787d0ff63ed55b2
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-15 09:35:43 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header'; 
 import About from './components/About/About';
-import HeaderMe from './components/HeaderMe/HeaderMe'
+import HeaderMe from './components/HeaderMe/HeaderMe';
+import Services from './components/service/Services';
+
 function App() {
   return (
     <>
@@ -9,6 +11,7 @@ function App() {
       <h1>React portfolio</h1>
       <HeaderMe/>
       <About/>
+      <Services/>
       <Footer/>
     </>
   );
